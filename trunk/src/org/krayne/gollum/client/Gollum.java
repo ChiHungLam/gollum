046d76a8c43b175572e4b90b545a8f5ba874ebb2
hsu.dennis <hsu.dennis@ee8fcde9-514b-0410-8cff-c54a1ecd2d70>
2008-05-10 22:41:18 +0000

    Renamed MainUi to GollumPanel

--- trunk/src/org/krayne/gollum/client/Gollum.java
+++ trunk/src/org/krayne/gollum/client/Gollum.java
@@ -5,7 +5,7 @@ import org.krayne.gollum.client.bookmarks.StaticBookmark;
 import org.krayne.gollum.client.debug.DebugBookmarksSource;
 import org.krayne.gollum.client.map.LatLon;
 import org.krayne.gollum.client.map.Map;
-import org.krayne.gollum.client.ui.MainUi;
+import org.krayne.gollum.client.ui.GollumPanel;
 import org.krayne.gollum.client.ui.controlpanel.bookmarkssection.BookmarksSection;
 
 import com.google.gwt.core.client.EntryPoint;
@@ -17,25 +17,25 @@ import com.google.gwt.user.client.ui.RootPanel;
  */
 public class Gollum implements EntryPoint {
     private static final Bookmark DEFAULT_START_BOOKMARK = new StaticBookmark("Start", new LatLon(34.055389, -118.249789), new Integer(3));
-    private final MainUi ui;
+    private final GollumPanel gollumPanel;
     
     public Gollum() {
-        this.ui = new MainUi();
-        Map map = this.ui.getMap();
+        this.gollumPanel = new GollumPanel();
+        Map map = this.gollumPanel.getMap();
         
         BookmarksSection bookmarksSection = new BookmarksSection(map);
         bookmarksSection.addSource(new DebugBookmarksSource());
-        this.ui.addControlPanelSection(bookmarksSection);
+        this.gollumPanel.addControlPanelSection(bookmarksSection);
         
-        this.ui.addTab("Test Tab", new Label("Just a test tab"));
+        this.gollumPanel.addTab("Test Tab", new Label("Just a test tab"));
     }
     
     /**
      * This is the entry point method.
      */
     public void onModuleLoad() {
-        RootPanel.get("gollum").add(this.ui);
-        Map map = this.ui.getMap();
+        RootPanel.get("gollum").add(this.gollumPanel);
+        Map map = this.gollumPanel.getMap();
         map.jumpTo(DEFAULT_START_BOOKMARK);
     }
 

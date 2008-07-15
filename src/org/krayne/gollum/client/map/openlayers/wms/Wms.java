0d8293d4ccb579b2538f45993c326f1a139a2f78
stphung <stphung@ee8fcde9-514b-0410-8cff-c54a1ecd2d70>
2008-07-15 22:36:11 +0000

    Added support for OpenLayers Wms Layers, GollumPanel now takes in an OpenLayersMapWidget in the constructor instead of using a DefaultOpenLayersMapWidget by default.

--- src/org/krayne/gollum/client/map/openlayers/wms/Wms.java
+++ src/org/krayne/gollum/client/map/openlayers/wms/Wms.java
@@ -4,16 +4,43 @@ import org.krayne.gollum.client.map.openlayers.Layer;
 
 import com.google.gwt.core.client.JavaScriptObject;
 
-public class Wms {
-    public static class BasicLayer extends Layer {
-        public BasicLayer(String layerName) {
-            super(_createBasicLayer(layerName));
-        }
+/**
+ * An OpenLayers Wms Layer that we can access in Java.
+ * 
+ * @author stphung
+ */
+public class Wms extends Layer {
+    
+    public Wms(String layerName, String wmsUrl, WmsProperties wmsProperties) {
+        super(_createWmsLayer(layerName, wmsUrl,
+                wmsProperties.getHeight() == 0 ? "" : Integer.toString(wmsProperties.getHeight()),
+                wmsProperties.getWidth() == 0 ? "" : Integer.toString(wmsProperties.getWidth()),
+                wmsProperties.getLayers() == null ? "" : wmsProperties.getLayers(),
+                wmsProperties.getStyles() == null ? "" : wmsProperties.getStyles(),
+                wmsProperties.getSrs() == null ? "" : wmsProperties.getSrs(),
+                wmsProperties.getFormat() == null ? "" : wmsProperties.getFormat(),
+                wmsProperties.isTiled() ? "true" : "",
+                
+                wmsProperties.getTilesOrigin() == null ? 
+                		"" : wmsProperties.getTilesOrigin().getLonDegrees() + "," + wmsProperties.getTilesOrigin().getLatDegrees()
+                ));
     }
-    
-    //--------------------------------------------------------------------------
-    
-    private static native JavaScriptObject _createBasicLayer(String layerName) /*-{
-        return new $wnd.OpenLayers.Layer.WMS(layerName, "http://labs.metacarta.com/wms/vmap0?", {layers: 'basic'});
+
+    // --------------------------------------------------------------------------
+
+    private static native JavaScriptObject _createWmsLayer(String layerName, String wmsUrl, 
+    		String height, String width, String layers, String styles, 
+    		String srs, String format, String isTiled, String tilesOrigin) /*-{
+        var params = {};
+        if ( height != "" ) params["height"] = height;
+        if ( width != "" ) params["width"] = width;
+        if ( layers != "" ) params["layers"] = layers;
+        if ( styles != "" ) params["styles"] = styles;
+        if ( srs != "" ) params["srs"] = srs;
+        if ( format != "" ) params["format"] = format;
+        if ( isTiled != "" ) params["tiled"] = isTiled;
+        if ( tilesOrigin != "" ) params["tilesOrigin"] = tilesOrigin;
+        
+        return new $wnd.OpenLayers.Layer.WMS(layerName, wmsUrl, params);
     }-*/;
 }
